a1ab72d23d7f8797a3a04b415b3d63978a7b8195
danthebk <danthebk@gmail.com>
2020-07-31 09:42:12 -0400

    quick-food selections

--- FFXIV_DPS/Scripts/dps.js
+++ FFXIV_DPS/Scripts/dps.js
@@ -23,6 +23,241 @@ dps_app.controller("dps_controller", function ($scope, $http) {
             { tier: "V - 12", value: 12 }
         ]
 
+        //initialize food
+        $scope.food = [
+            {
+                name: "None",
+                criticalhitValue: 0,
+                criticalhitPercent: 0,
+                criticalhitCap: 0,
+                determinationValue: 0,
+                determinationPercent: 0,
+                determinationCap: 0,
+                directhitValue: 0,
+                directhitPercent: 0,
+                directhitCap: 0,
+                pietyValue: 0,
+                pietyPercent: 0,
+                pietyCap: 0,
+                skillspeedValue: 0,
+                skillspeedPercent: 0,
+                skillspeedCap: 0,
+                tenacityValue: 0,
+                tenacityPercent: 0,
+                tenacityCap: 0
+            },
+            {
+                name: "Farmer's Breakfast HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 0,
+                criticalhitCap: 0,
+                determinationValue: 0,
+                determinationPercent: 10,
+                determinationCap: 101,
+                directhitValue: 0,
+                directhitPercent: 0,
+                directhitCap: 0,
+                pietyValue: 0,
+                pietyPercent: 0,
+                pietyCap: 0,
+                skillspeedValue: 0,
+                skillspeedPercent: 0,
+                skillspeedCap: 0,
+                tenacityValue: 0,
+                tenacityPercent: 10,
+                tenacityCap: 168
+            },
+            {
+                name: "Herring Pie HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 10,
+                criticalhitCap: 168,
+                determinationValue: 0,
+                determinationPercent: 0,
+                determinationCap: 0,
+                directhitValue: 0,
+                directhitPercent: 0,
+                directhitCap: 0,
+                pietyValue: 0,
+                pietyPercent: 0,
+                pietyCap: 0,
+                skillspeedValue: 0,
+                skillspeedPercent: 10,
+                skillspeedCap: 101,
+                tenacityValue: 0,
+                tenacityPercent: 0,
+                tenacityCap: 0
+            },
+            {
+                name: "Honey Croissant HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 0,
+                criticalhitCap: 0,
+                determinationValue: 0,
+                determinationPercent: 0,
+                determinationCap: 0,
+                directhitValue: 0,
+                directhitPercent: 0,
+                directhitCap: 0,
+                pietyValue: 0,
+                pietyPercent: 10,
+                pietyCap: 101,
+                skillspeedValue: 0,
+                skillspeedPercent: 10,
+                skillspeedCap: 168,
+                tenacityValue: 0,
+                tenacityPercent: 0,
+                tenacityCap: 0
+            },
+            {
+                name: "Lemon Waffle HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 0,
+                criticalhitCap: 0,
+                determinationValue: 0,
+                determinationPercent: 10,
+                determinationCap: 101,
+                directhitValue: 0,
+                directhitPercent: 0,
+                directhitCap: 0,
+                pietyValue: 0,
+                pietyPercent: 10,
+                pietyCap: 168,
+                skillspeedValue: 0,
+                skillspeedPercent: 0,
+                skillspeedCap: 0,
+                tenacityValue: 0,
+                tenacityPercent: 0,
+                tenacityCap: 0
+            },
+            {
+                name: "Mors HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 0,
+                criticalhitCap: 0,
+                determinationValue: 0,
+                determinationPercent: 0,
+                determinationCap: 0,
+                directhitValue: 0,
+                directhitPercent: 10,
+                directhitCap: 101,
+                pietyValue: 0,
+                pietyPercent: 0,
+                pietyCap: 0,
+                skillspeedValue: 0,
+                skillspeedPercent: 10,
+                skillspeedCap: 168,
+                tenacityValue: 0,
+                tenacityPercent: 0,
+                tenacityCap: 0
+            },
+            {
+                name: "Ovim Meatballs HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 0,
+                criticalhitCap: 0,
+                determinationValue: 0,
+                determinationPercent: 10,
+                determinationCap: 101,
+                directhitValue: 0,
+                directhitPercent: 10,
+                directhitCap: 168,
+                pietyValue: 0,
+                pietyPercent: 0,
+                pietyCap: 0,
+                skillspeedValue: 0,
+                skillspeedPercent: 0,
+                skillspeedCap: 0,
+                tenacityValue: 0,
+                tenacityPercent: 0,
+                tenacityCap: 0
+            },
+            {
+                name: "Pickled Herring HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 0,
+                criticalhitCap: 0,
+                determinationValue: 0,
+                determinationPercent: 0,
+                determinationCap: 0,
+                directhitValue: 0,
+                directhitPercent: 10,
+                directhitCap: 101,
+                pietyValue: 0,
+                pietyPercent: 0,
+                pietyCap: 0,
+                skillspeedValue: 0,
+                skillspeedPercent: 10,
+                skillspeedCap: 168,
+                tenacityValue: 0,
+                tenacityPercent: 0,
+                tenacityCap: 0
+            },
+            {
+                name: "Sausage and Sauerkraut HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 10,
+                criticalhitCap: 168,
+                determinationValue: 0,
+                determinationPercent: 0,
+                determinationCap: 0,
+                directhitValue: 0,
+                directhitPercent: 0,
+                directhitCap: 0,
+                pietyValue: 0,
+                pietyPercent: 0,
+                pietyCap: 0,
+                skillspeedValue: 0,
+                skillspeedPercent: 10,
+                skillspeedCap: 101,
+                tenacityValue: 0,
+                tenacityPercent: 0,
+                tenacityCap: 0
+            },
+            {
+                name: "Silkie Pudding HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 0,
+                criticalhitCap: 0,
+                determinationValue: 0,
+                determinationPercent: 10,
+                determinationCap: 168,
+                directhitValue: 0,
+                directhitPercent: 0,
+                directhitCap: 0,
+                pietyValue: 0,
+                pietyPercent: 0,
+                pietyCap: 0,
+                skillspeedValue: 0,
+                skillspeedPercent: 0,
+                skillspeedCap: 0,
+                tenacityValue: 0,
+                tenacityPercent: 10,
+                tenacityCap: 101
+            },
+            {
+                name: "Stuffed Highland Cabbage HQ",
+                criticalhitValue: 0,
+                criticalhitPercent: 10,
+                criticalhitCap: 101,
+                determinationValue: 0,
+                determinationPercent: 10,
+                determinationCap: 168,
+                directhitValue: 0,
+                directhitPercent: 0,
+                directhitCap: 0,
+                pietyValue: 0,
+                pietyPercent: 0,
+                pietyCap: 0,
+                skillspeedValue: 0,
+                skillspeedPercent: 0,
+                skillspeedCap: 0,
+                tenacityValue: 0,
+                tenacityPercent: 0,
+                tenacityCap: 0
+            }
+        ]
+
         //index values for each stat; used to simplify the quick-materia buttons
         $scope.stats_indexes = {
             critical: 0,
@@ -88,6 +323,12 @@ dps_app.controller("dps_controller", function ($scope, $http) {
         //the player's stats
         //also the player's calculated stats
         $scope.dps_stats = {
+            food_selected: $scope.food[0],
+            food_stat1: "",
+            food_value1: "",
+            food_stat2: "",
+            food_value2: "",
+
             determination: 340,
             directhit: 380,
             tenacity: 380,
@@ -129,27 +370,143 @@ dps_app.controller("dps_controller", function ($scope, $http) {
     //primary functions; calulation and ui
     //-----------------------------------------------
 
+    //food calculations
+    $scope.food_calculate = function () {
+        //critical hit
+        $scope.dps_stats.food_selected.criticalhitValue = $scope.dps_stats.critical * ($scope.dps_stats.food_selected.criticalhitPercent / 100);
+        if ($scope.dps_stats.food_selected.criticalhitValue > $scope.dps_stats.food_selected.criticalhitCap) {
+            $scope.dps_stats.food_selected.criticalhitValue = $scope.dps_stats.food_selected.criticalhitCap;
+        }
+
+        //determination
+        $scope.dps_stats.food_selected.determinationValue = $scope.dps_stats.determination * ($scope.dps_stats.food_selected.determinationPercent / 100);
+        if ($scope.dps_stats.food_selected.determinationValue > $scope.dps_stats.food_selected.determinationCap) {
+            $scope.dps_stats.food_selected.determinationValue = $scope.dps_stats.food_selected.determinationCap;
+        }
+
+        //direct hit
+        $scope.dps_stats.food_selected.directhitValue = $scope.dps_stats.directhit * ($scope.dps_stats.food_selected.directhitPercent / 100);
+        if ($scope.dps_stats.food_selected.directhitValue > $scope.dps_stats.food_selected.directhitCap) {
+            $scope.dps_stats.food_selected.directhitValue = $scope.dps_stats.food_selected.directhitCap;
+        }
+
+        //skill speed
+        $scope.dps_stats.food_selected.skillspeedValue = $scope.dps_stats.skillspeed * ($scope.dps_stats.food_selected.skillspeedPercent / 100);
+        if ($scope.dps_stats.food_selected.skillspeedValue > $scope.dps_stats.food_selected.skillspeedCap) {
+            $scope.dps_stats.food_selected.skillspeedValue = $scope.dps_stats.food_selected.skillspeedCap;
+        }
+
+        //tenacity
+        $scope.dps_stats.food_selected.tenacityValue = $scope.dps_stats.tenacity * ($scope.dps_stats.food_selected.tenacityPercent / 100);
+        if ($scope.dps_stats.food_selected.tenacityValue > $scope.dps_stats.food_selected.tenacityCap) {
+            $scope.dps_stats.food_selected.tenacityValue = $scope.dps_stats.food_selected.tenacityCap;
+        }
+    }
+
+    //food ui
+    $scope.food_ui = function () {
+        $scope.dps_stats.food_stat1 = "";
+        $scope.dps_stats.food_value1 = "";
+        $scope.dps_stats.food_stat2 = "";
+        $scope.dps_stats.food_value2 = "";
+
+        var second = false;
+
+        //critical hit
+        if ($scope.dps_stats.food_selected.criticalhitCap > 0) {
+            if (second) {
+                $scope.dps_stats.food_stat2 = "Critical Hit: ";
+                $scope.dps_stats.food_value2 = $scope.dps_stats.food_selected.criticalhitValue;
+            } else {
+                $scope.dps_stats.food_stat1 = "Critical Hit: ";
+                $scope.dps_stats.food_value1 = $scope.dps_stats.food_selected.criticalhitValue;
+                second = true;
+            }
+        }
+
+        //determination
+        if ($scope.dps_stats.food_selected.determinationCap > 0) {
+            if (second) {
+                $scope.dps_stats.food_stat2 = "Determination: ";
+                $scope.dps_stats.food_value2 = $scope.dps_stats.food_selected.determinationValue;
+            } else {
+                $scope.dps_stats.food_stat1 = "Determination: ";
+                $scope.dps_stats.food_value1 = $scope.dps_stats.food_selected.determinationValue;
+                second = true;
+            }
+        }
+
+        //direct hit
+        if ($scope.dps_stats.food_selected.directhitCap > 0) {
+            if (second) {
+                $scope.dps_stats.food_stat2 = "Direct Hit: ";
+                $scope.dps_stats.food_value2 = $scope.dps_stats.food_selected.directhitValue;
+            } else {
+                $scope.dps_stats.food_stat1 = "Direct Hit: ";
+                $scope.dps_stats.food_value1 = $scope.dps_stats.food_selected.directhitValue;
+                second = true;
+            }
+        }
+
+        //mostly unimplemented since it doesn't affect dps, just going to display the cap
+        //piety
+        if ($scope.dps_stats.food_selected.pietyCap > 0) {
+            if (second) {
+                $scope.dps_stats.food_stat2 = "Piety: ";
+                $scope.dps_stats.food_value2 = $scope.dps_stats.food_selected.pietyCap;
+            } else {
+                $scope.dps_stats.food_stat1 = "Piety: ";
+                $scope.dps_stats.food_value1 = $scope.dps_stats.food_selected.pietyCap;
+                second = true;
+            }
+        }
+
+        //skill speed
+        if ($scope.dps_stats.food_selected.skillspeedCap > 0) {
+            if (second) {
+                $scope.dps_stats.food_stat2 = "Skill/Spell Speed: ";
+                $scope.dps_stats.food_value2 = $scope.dps_stats.food_selected.skillspeedValue;
+            } else {
+                $scope.dps_stats.food_stat1 = "Skill/Spell Speed: ";
+                $scope.dps_stats.food_value1 = $scope.dps_stats.food_selected.skillspeedValue;
+                second = true;
+            }
+        }
+
+        //tenacity
+        if ($scope.dps_stats.food_selected.tenacityCap > 0) {
+            if (second) {
+                $scope.dps_stats.food_stat2 = "Tenacity: ";
+                $scope.dps_stats.food_value2 = $scope.dps_stats.food_selected.tenacityValue;
+            } else {
+                $scope.dps_stats.food_stat1 = "Tenacity: ";
+                $scope.dps_stats.food_value1 = $scope.dps_stats.food_selected.tenacityValue;
+                second = true;
+            }
+        }
+    }
+
     //stat calculations
     $scope.stats_calculate = function () {
         //critical hit
-        $scope.dps_stats.criticalDelta = $scope.dps_stats.critical - $scope.dps_base.levelBase;
+        $scope.dps_stats.criticalDelta = $scope.dps_stats.critical - $scope.dps_base.levelBase + $scope.dps_stats.food_selected.criticalhitValue;
         $scope.dps_stats.criticalRate = ($scope.dps_stats.criticalDelta / $scope.dps_base.criticalRatio) * .1;
         $scope.dps_stats.criticalDamage = ($scope.dps_stats.criticalDelta / $scope.dps_base.criticalRatio) * .1;
         $scope.dps_stats.criticalDPS = (($scope.dps_base.criticalBaseRate + $scope.dps_stats.criticalRate) / 100) * ($scope.dps_base.criticalBaseDamage + $scope.dps_stats.criticalDamage);
 
         //determination and tenacity
-        $scope.dps_stats.determinationDelta = $scope.dps_stats.determination - ($scope.dps_base.levelBase - 40); //determination inexplicably has 40 less base value
+        $scope.dps_stats.determinationDelta = $scope.dps_stats.determination - ($scope.dps_base.levelBase - 40) + $scope.dps_stats.food_selected.determinationValue; //determination inexplicably has 40 less base value
         $scope.dps_stats.determinationDPS = ($scope.dps_stats.determinationDelta / $scope.dps_base.determinationRatio) * .1;
-        $scope.dps_stats.tenacityDelta = $scope.dps_stats.tenacity - $scope.dps_base.levelBase;
+        $scope.dps_stats.tenacityDelta = $scope.dps_stats.tenacity - $scope.dps_base.levelBase + $scope.dps_stats.food_selected.tenacityValue;
         $scope.dps_stats.tenacityDPS = ($scope.dps_stats.tenacityDelta / $scope.dps_base.tenacityRatio) * .1;
 
         //direct hit
-        $scope.dps_stats.directhitDelta = $scope.dps_stats.directhit - $scope.dps_base.levelBase;
+        $scope.dps_stats.directhitDelta = $scope.dps_stats.directhit - $scope.dps_base.levelBase + $scope.dps_stats.food_selected.directhitValue;
         $scope.dps_stats.directhitRate = ($scope.dps_stats.directhitDelta / $scope.dps_base.directhitRatio) * .1;
         $scope.dps_stats.directhitDPS = $scope.dps_stats.directhitRate * ($scope.dps_base.directhitBaseDamage / 100);
 
         //skill speed
-        $scope.dps_stats.skillspeedDelta = $scope.dps_stats.skillspeed - $scope.dps_base.levelBase;
+        $scope.dps_stats.skillspeedDelta = $scope.dps_stats.skillspeed - $scope.dps_base.levelBase + $scope.dps_stats.food_selected.skillspeedValue;
         $scope.dps_stats.skillspeedDelay = Math.floor(130 * $scope.dps_stats.skillspeedDelta / $scope.dps_base.skillspeedLevelMod);
         $scope.dps_stats.skillspeedDelay = 1000 - $scope.dps_stats.skillspeedDelay;
         $scope.dps_stats.skillspeedDelay = Math.floor($scope.dps_stats.skillspeedDelay * ($scope.dps_base.skillspeedBase * 1000) / 1000);
@@ -250,6 +607,9 @@ dps_app.controller("dps_controller", function ($scope, $http) {
 
     //user adjusted their stats, recalculate and update the ui
     $scope.stats_change = function () {
+        $scope.food_calculate();
+        $scope.food_ui();
+
         $scope.stats_calculate();
         $scope.stats_ui();
     }
@@ -261,6 +621,12 @@ dps_app.controller("dps_controller", function ($scope, $http) {
     //save the main object to the archive
     $scope.archive_save = function () {
         $scope.dps_stats_archive = {
+            food_selected: $scope.dps_stats.food_selected,
+            food_stat1: $scope.dps_stats.food_stat1,
+            food_value1: $scope.dps_stats.food_value1,
+            food_stat2: $scope.dps_stats.food_stat2,
+            food_value2: $scope.dps_stats.food_value2,
+
             determination: $scope.dps_stats.determination,
             directhit: $scope.dps_stats.directhit,
             tenacity: $scope.dps_stats.tenacity,
@@ -296,6 +662,12 @@ dps_app.controller("dps_controller", function ($scope, $http) {
     //load the main object with the archived data
     $scope.archive_revert = function () {
         $scope.dps_stats = {
+            food_selected: $scope.dps_stats_archive.food_selected,
+            ood_stat1: $scope.dps_stats_archive.food_stat1,
+            food_value1: $scope.dps_stats_archive.food_value1,
+            food_stat2: $scope.dps_stats_archive.food_stat2,
+            food_value2: $scope.dps_stats_archive.food_value2,
+
             determination: $scope.dps_stats_archive.determination,
             directhit: $scope.dps_stats_archive.directhit,
             tenacity: $scope.dps_stats_archive.tenacity,
