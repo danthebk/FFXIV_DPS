a270fd44e94cac3b6e468e3ab710a8ff1b39e2e2
danthebk <danthebk@gmail.com>
2020-08-02 21:54:21 -0400

    split angular controller into multiple files

---
+++ FFXIV_DPS/Scripts/dps_main.js
@@ -0,0 +1,10 @@
+var jsmodel;
+
+var dps_app = angular.module("dps_app", []);
+dps_app.controller("dps_controller", function ($scope, $http) {
+    //initialize functions
+    dps_app.dps_controller($scope);
+
+    //initialize models
+    dps_app.dps_model($scope);
+})
